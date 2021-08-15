e7050fa1614b98629c3eed4096d35200528274df
kaayssmile <kaay.hertenstein@gmail.com>
2021-08-15 17:35:16 +0200

    Progress Responive, Upload JS

---
+++ JS/JS_InvoiceApp.js
@@ -0,0 +1,138 @@
+/* Invoice App */ 
+
+
+/* Imports  */
+
+import * as Tools from "./Modules/Functions.js";
+
+/* DOM Selectors Wrappers */
+
+var wrapper_content = document.querySelector(".wrapper_content");
+var wrapper_main = document.querySelector(".wrapper_main");
+var wrapper_NewandEditInvoice = document.querySelectorAll(".wrapper_newandeditInvoice");
+var wrapper_NewInvoice = wrapper_NewandEditInvoice[0];
+var wrapper_EditInvoice = wrapper_NewandEditInvoice[1];
+var wrapper_ViewInvoice = document.querySelector(".wrapper_Viewinvoice");
+
+var wrapper_content = document.querySelector(".wrapper_content");
+
+/* DOM Selectors Menu */
+
+
+
+
+
+var filter_box = document.querySelector(".header__title--filter");
+console.log(filter_box);
+
+
+/* Event Listeners */
+
+wrapper_content.addEventListener("click", delegation_InvoiceApp);
+wrapper_NewInvoice.addEventListener("click", delegation_NewInvoice);
+
+
+
+
+
+
+
+
+
+/* Functions*/
+
+function delegation_NewInvoice(event){
+
+    var element = event.target;
+
+    if(element.matches(".delete_item")){
+
+        element.parentNode.parentNode.parentNode.remove();
+        
+    }
+    if(element.matches(".delete_item--path")){
+
+        element.parentNode.parentNode.parentNode.parentNode.remove();
+
+    }
+    if(element.matches(".wrapper_newandeditInvoice__form--button")){
+
+        let id = element.previousElementSibling.getAttribute("data-id");
+        Tools.createnewitem(id);
+
+    }
+    if(element.matches(".wrapper_newandeditInvoice__form--text")){
+
+        if(element.parentNode.classList == "wrapper_newandeditInvoice__form--button"){
+
+            let id = element.parentNode.previousElementSibling.getAttribute("data-id");
+            Tools.createnewitem(id);
+
+        }
+
+    }
+    if(element.matches(".wrapper_newandeditInvoice__form--buttonsmall") || element.matches(".discard")){
+        
+        console.log("clik");
+        Tools.deleteallInputs();
+
+    }
+
+}
+
+
+
+
+
+
+
+
+function delegation_InvoiceApp(event){
+
+    var element = event.target;
+
+
+/*     Menu Delegation     */
+
+    if(element.matches(".menu__itemtop") || element.matches(".menu__itemtop--svg") || element.matches(".menu__itemtop--svgpath")){
+        
+        wrapper_main.classList.remove("disable");
+        wrapper_NewInvoice.classList.remove("enable");
+        wrapper_ViewInvoice.classList.remove("enable");
+        wrapper_EditInvoice.classList.remove("enable");
+
+        wrapper_main.classList.add("enable");
+        wrapper_NewInvoice.classList.add("disable");
+        wrapper_ViewInvoice.classList.add("disable");
+        wrapper_EditInvoice.classList.add("disable");
+
+    }
+
+/*     Main Delegation     */
+
+    if(element.matches(".header__btn") || element.matches(".header__btn--text") || element.matches(".header__btn--circle")){
+
+        wrapper_main.classList.remove("enable");
+        wrapper_main.classList.add("disable");
+        wrapper_NewInvoice.classList.remove("disable");
+        wrapper_NewInvoice.classList.add("enable");
+
+    }
+    if(element.matches(".main__invoice") || element.matches(".main__invoice--check") || element.matches(".main__invoice--checkpath")){
+
+        wrapper_main.classList.remove("enable");
+        wrapper_main.classList.add("disable");
+        wrapper_ViewInvoice.classList.remove("disable");
+        wrapper_ViewInvoice.classList.add("enable");
+
+    }
+    if(element.matches(".header__select--svg")){
+
+        filter_box.style.backgroundColor = green; // ...? 
+
+    }
+
+/*     NewInvoice Delegation     */
+
+   
+}
---
+++ JS/Modules/Functions.js
@@ -0,0 +1,73 @@
+
+
+
+
+
+
+function deleteallInputs(){
+
+    var allinputs = document.querySelectorAll("input");
+    console.log(allinputs);
+
+    
+
+
+    for(input of allinputs){
+
+        input.value = "";
+    }
+
+}
+
+
+
+
+
+
+
+
+function createnewitem(id){
+
+
+    var wrappers_InvoiceForm = document.querySelectorAll(".wrapper_newandeditInvoice__form");
+    var wrapper_NewInvoiceForm = wrappers_InvoiceForm[0];
+
+    var button_newItem = document.querySelector(".wrapper_newandeditInvoice__form--button");
+
+    let idneu = parseInt(id) + 1;
+    let item_div = document.createElement("div");
+    item_div.classList.add("wrapper_newandeditInvoice__form--item");
+    item_div.setAttribute("data-id", idneu);
+
+    let names_div = document.createElement("div");
+    names_div.classList.add("wrapper_newandeditInvoice__form--flex", "wrap");
+
+    names_div.innerHTML = `<p class="wrapper_newandeditInvoice__form--text width40">Item Name</p>
+                        <input type="text" placeholder="Banner Design"  id="item${idneu}_name" class="wrapper_newandeditInvoice__form--inputmobile width40">
+                        <p class="wrapper_newandeditInvoice__form--text width10">Qty.</p>
+                        <p class="wrapper_newandeditInvoice__form--text width20 ">Price</p>
+                        <p class="wrapper_newandeditInvoice__form--text width19 ">Total</p>
+                        <p class="wrapper_newandeditInvoice__form--textpseudo "></p>`;
+
+    let inputs_div = document.createElement("div");
+    inputs_div.classList.add("wrapper_newandeditInvoice__form--flex");
+
+    inputs_div.innerHTML = `<input type="text" placeholder="Service Name"  id="item${idneu}_name" class="wrapper_newandeditInvoice__form--inputdesktop width40">
+                            <input type="text" placeholder="Services Count"  id="item${idneu}_Qty" class="wrapper_newandeditInvoice__form--input width10">
+                            <input type="text" placeholder="Service Price"  id="item${idneu}_price" class="wrapper_newandeditInvoice__form--input width20">
+        
+                            <p class="wrapper_newandeditInvoice__form--textsized ">400.00</p>
+        
+                            <div class="wrapper_newandeditInvoice__form--flextext">
+
+                                <svg class="delete_item" width="13" height="16" xmlns="http://www.w3.org/2000/svg"><path class="delete_item--path" d="M11.583 3.556v10.666c0 .982-.795 1.778-1.777 1.778H2.694a1.777 1.777 0 01-1.777-1.778V3.556h10.666zM8.473 0l.888.889h3.111v1.778H.028V.889h3.11L4.029 0h4.444z" fill="#888EB0" fill-rule="nonzero"/>
+                            </svg>`;
+
+    item_div.appendChild(names_div);
+    item_div.appendChild(inputs_div);
+
+    wrapper_NewInvoiceForm.insertBefore(item_div, button_newItem);
+
+}
+
+export {createnewitem,deleteallInputs};
